851db045675f25dc95fb412d62d882f98eda07cb
xingguo <xingguo@mafengwo.com>
2023-11-07 11:43:03 +0800

    update

--- x/grpc/tls/tls_gen.sh
+++ x/grpc/tls/tls_gen.sh
@@ -2,15 +2,18 @@
 #根证书 
 subj="/C=GB/L=BeiJing/O=Personal/CN=XG"
 days=36500
-openssl genrsa -out pem/ca.key 2048
-openssl req -new -x509 -days $days -subj "$subj" -key pem/ca.key -out pem/ca.crt 
+pemdir=pem
+confdir=conf
+openssl genrsa -out $pemdir/ca.key 2048
+openssl req -new -x509 -days $days -subj "$subj" -key $pemdir/ca.key -out $pemdir/ca.crt 
 #服务端证书
-openssl genrsa -out pem/server.key 2048
-openssl req -new -subj "$subj" -key pem/server.key -out pem/server.csr 
-openssl x509 -req -sha256 -CA pem/ca.crt -CAkey pem/ca.key -CAcreateserial -days $days -in pem/server.csr -out pem/server.crt -extensions req_ext -extfile conf/sans.conf 
+openssl genrsa -out $pemdir/server.key 2048
+#echo "openssl req -new -subj $subj -key $pemdir/server.key -out $pemdir/server.csr" 
+openssl req -new -subj "$subj" -key $pemdir/server.key -out $pemdir/server.csr 
+openssl x509 -req -sha256 -CA $pemdir/ca.crt -CAkey $pemdir/ca.key -CAcreateserial -days $days -in $pemdir/server.csr -out $pemdir/server.crt -extensions req_ext -extfile $confdir/san.conf 
 
 #客户端证书
-openssl genrsa -out pem/client.key 2048
-openssl req -new -subj "$subj" -key pem/client.key -out pem/client.csr 
-openssl x509 -req -sha256 -CA pem/ca.crt -CAkey pem/ca.key -CAcreateserial -days $days -in pem/client.csr -out pem/client.crt -extensions req_ext -extfile conf/san.conf 
-rm -f pem/server.csr pem/client.csr
+openssl genrsa -out $pemdir/client.key 2048
+openssl req -new -subj "$subj" -key $pemdir/client.key -out $pemdir/client.csr 
+openssl x509 -req -sha256 -CA $pemdir/ca.crt -CAkey $pemdir/ca.key -CAcreateserial -days $days -in $pemdir/client.csr -out $pemdir/client.crt -extensions req_ext -extfile $confdir/san.conf 
+rm -f $pemdir/server.csr $pemdir/client.csr   

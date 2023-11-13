3f802001973a3e1be28df108454ef8dc332f53df
xingguo <xingguo@mafengwo.com>
2023-11-13 19:11:00 +0800

    duration

--- x/time_test.go
+++ x/time_test.go
@@ -13,3 +13,19 @@ func TestDuration(t *testing.T) {
 	duration := time.Now().Sub(t1)
 	fmt.Printf("sleep %d秒，历时%d秒\n", deleySecond, int(duration.Seconds()))
 }
+func TestTimer(t *testing.T) {
+	i := 0
+	d := time.Duration(1) * time.Second
+	timer := time.NewTimer(d)
+	defer timer.Stop()
+	for {
+		select {
+		case <-timer.C:
+			i++
+			//每秒执行1次
+			fmt.Printf("timer 1s %d\n", i)
+			//循环与internal类似
+			timer.Reset(d)
+		}
+	}
+}

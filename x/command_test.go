11193779a96114b29f2482a358a1629b075e630c
xingguo <xingguo@mafengwo.com>
2024-08-01 18:27:22 +0800

    test

--- x/command_test.go
+++ x/command_test.go
@@ -1,6 +1,8 @@
 package x
 
 import (
+	"fmt"
+	"os"
 	"os/exec"
 	"strings"
 	"testing"
@@ -21,4 +23,32 @@ func cmdOutput() bool {
 func TestCmd(t *testing.T) {
 	assert.Equal(t, nil, cmdNoOutput("180.76.76.76"))
 	assert.Equal(t, true, cmdOutput())
+}
+func TestCmdInput(t *testing.T) {
+	r, w, err := os.Pipe()
+	if err != nil {
+		fmt.Println("Error creating pipe:", err)
+		return
+	}
+	go func() {
+		defer w.Close()
+		fmt.Fprintln(w, "Hello from the findnotme!")
+		fmt.Fprintln(w, "Hello from the findme!")
+	}()
+
+	cmd := exec.Command("grep", "findme") // grep 命令会从标准输入中读取并查找包含 "findme" 的行
+
+	// 将标准输入传递给子进程
+	cmd.Stdin = r
+
+	// 执行子进程
+	output, err := cmd.CombinedOutput()
+	if err != nil {
+		fmt.Println("Error:", err)
+		return
+	}
+
+	// 打印子进程的输出
+	fmt.Println("Output from child process:")
+	fmt.Println(string(output))
 }
